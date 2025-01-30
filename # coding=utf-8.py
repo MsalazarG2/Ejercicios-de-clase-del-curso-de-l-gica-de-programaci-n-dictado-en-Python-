56e1ee1352231eaa212875b0f248ddca11ae1fdc
MsalazarG2 <miguel.salazar.ga@gmail.com>
2025-01-30 09:33:57 -0500

    Add files via upload

---
+++ # coding=utf-8.py
@@ -0,0 +1,70 @@
+# coding=utf-8
+
+
+# Este es un comentario que no se ejecuta
+print('Hola Ciudad de Sabaneta') #Aqui hay otro comentario
+#Caracteres 
+#simbolos: !@#$%^&*()_+{}|:"<>?[]\;',.
+#cifras: 1234567890
+#letras: abcdefghijklmnopqrstuvwxyz ñ á é í ó ú	
+
+
+print('3.14159265')
+
+#variables: son definiciopnes de datos que pueden cambiar su valor
+#declarar una variable: Es asignarle el nombre de la variable y darle un valor
+#la asignacion se hace con el signo igual
+#la asignacion no es conmutativa. primero va el nombre de la variable y luego el valor
+
+una_frase = '¡hola ciudad de itagüí!'
+numero_pi = 3.14159265
+p = 3.14159265
+
+otra_frase = 'Aqui vivi en mi infancia'
+
+#concatenacion: es la union de dos o mas cadenas de texto
+print(una_frase+'\t'+otra_frase)
+
+
+print('El valor de la variable numero_pi es: '+ '\n' + str(numero_pi))
+
+
+otra_frase2 = 'Esta es la tercera frase'
+print(otra_frase2)
+
+#operadores matematicos
+suma = 3 + 8
+resta = 15 - 7
+multiplicacion = 12 * 6
+division = 12 / 5
+division_entera = 12 // 2
+potencia = 2 ** 3
+raiz_cuadrada = 9 ** 0.5    
+raiz_cubica = 27 ** (1/3)
+
+print('El resultado de la suma es: '+str(suma))
+print(type(suma))
+print('El resultado de la resta es: '+str(resta))
+print(type(resta))
+print('El resultado de la multiplicacion es: '+str(multiplicacion))
+print(type(multiplicacion))
+print('El resultado de la division es: '+str(division))
+print(type(division))
+print('El resultado de la division entera es: '+str(division_entera))
+print(type(division_entera))
+print('El resultado de la potencia es: '+str(potencia))
+print(type(potencia))
+print('El resultado de la raiz cuadrada es: '+str(raiz_cuadrada))   
+print(type(raiz_cuadrada))  
+print('El resultado de la raiz cubica es: '+str(raiz_cubica))   
+print(type(raiz_cubica))    
+
+operacion_muy_larga = 3 + 8 - 15 * 7 / 12
+print('El resultado de la operacion muy larga es: '+str(operacion_muy_larga))   
+print(type(operacion_muy_larga))    
+
+#operadores logicos
+operacion_logica = 3 < 8
+print('El resultado de la operacion logica es: '+str(operacion_logica))
+print(type(operacion_logica))
+
